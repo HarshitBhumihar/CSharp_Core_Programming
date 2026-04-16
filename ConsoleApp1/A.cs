661ebaa69d1ac48bb48722a4d734621cb74e36c8
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-04-16 10:35:05 +0530

    [Harshit] Add. Practice

---
+++ ConsoleApp1/A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class A
+    {
+        internal static void fn()
+        {
+            int a = 15;
+            Console.WriteLine(a);
+        }
+        internal void fn1()
+        {
+            int b = 10;
+            Console.WriteLine(b);
+        }
+    }
+}
