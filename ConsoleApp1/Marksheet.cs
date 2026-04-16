5af255d6eb14653f2423244ca2e086f80d0b2a1e
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-04-16 10:46:26 +0530

    [Harshit] Add. matrkseet problem

---
+++ ConsoleApp1/Marksheet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Marksheet
+    {
+        internal void pass()
+        {
+            //int num = 25;
+            //switch (num % 2)
+            //{
+            //    case 0:
+            //        Console.WriteLine("even");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("odd");
+            //        break ;
+        //}
+            char ch = 'a';
+            switch (ch)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("vowel");
+                    break;
+                default:
+                    Console.WriteLine("consonant");
+                    break;
+
+            }
+
+            }
+        }
+        
+            
+    }
+
