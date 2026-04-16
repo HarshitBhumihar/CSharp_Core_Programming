780a1e77bcd33c31e2366da00aa2236bbc6c4821
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-04-16 16:34:15 +0530

    [Harshit] EmployeeWage

---
+++ ConsoleApp1/EmployeeWageProblem1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class EmployeeWageProblem1
+    {
+        static void Main(string[] args)
+        {
+            double basicPay, houseRent, da, pf, netSalary;
+
+            Console.Write("Enter Basic Pay: ");
+            basicPay = Convert.ToDouble(Console.ReadLine());
+
+
+            houseRent = basicPay * 0.20;
+            da = basicPay * 0.10;
+
+
+            pf = basicPay * 0.12;
+
+
+            netSalary = basicPay + houseRent + da - pf;
+
+            Console.WriteLine("houseRent = " + houseRent);
+            Console.WriteLine("DA = " + da);
+            Console.WriteLine("PF = " + pf);
+            Console.WriteLine("Net Salary = " + netSalary);
+        }
+    }
+
+}
